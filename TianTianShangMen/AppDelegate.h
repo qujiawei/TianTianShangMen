ae78fb7e34b84684908b94c62612486eec2621aa
qujiawei <qujiawei@ayi360.com>
2015-09-22 10:23:53 +0800

    Initial Commit

---
+++ TianTianShangMen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TianTianShangMen
+//
+//  Created by qujiawei on 15/9/22.
+//  Copyright (c) 2015年 JYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
